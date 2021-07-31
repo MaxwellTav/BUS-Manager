127e663c9e1b8f43fd005e56da6855e9a768158e
Maxwell Tavares Rocha <Maxwelltrt0709@gmail.com>
2021-07-31 07:13:58 -0400

    Primer Commit
    En este commit se está mandando la aplicación totalmente funcional, pero sin ninguna característica.
    
    Solo tiene la organización, nada de funcionamiento.

--- BUS-Manager/Forms/SplashScreen.Designer.cs
+++ BUS-Manager/Forms/SplashScreen.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace BUS_Manager
 {
-    partial class Form1
+    partial class Index
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,10 +29,19 @@ namespace BUS_Manager
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(952, 519);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Index";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
